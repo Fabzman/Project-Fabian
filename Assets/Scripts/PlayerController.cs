6d7afbebdb3a0cb570b87bfe93b8807603b29242
Fabzman <Fabian.Est.Figueroa@gmail.com>
2017-11-23 18:55:57 +1100

    Running Animation and Basic Forward Movement
    Player Character now moves right when Right Arrow pressed and run animation plays when key is held

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    public GameObject PlayerCharacter;
+    //private SpriteRenderer;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 	}
+
+    public float speed;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("Move", true);
+        }
+
+        else
+        {
+            animator.SetBool("Move", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //SpriteRenderer.SetBool("Flip", true);
+            animator.SetBool("Left", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+            
 	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb.velocity = movement * speed;
+    }
 }
